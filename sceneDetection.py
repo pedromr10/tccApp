f933020d0dfb33b73724b3109103d5eaab3b876d
Pedro Munhoz Rosin <pedromunhozrosin@gmail.com>
2025-08-13 18:03:54 -0300

    adicionando arquivo para deteccao de ambiente

---
+++ sceneDetection.py
@@ -0,0 +1,11 @@
+#IMPORTS:
+import os
+
+#MAIN CODE:
+
+#Cria uma pasta para colocar os arquivos de print da tela:
+#OBS: Atualmente servirá apenas para colocar imagens para teste ainda nao vai fazer a captura.
+dir = "Gameplay"
+os.makedirs(dir, exist_ok=True)
+
+#Deteccao de objetos na cena:
