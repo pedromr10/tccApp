333caa63d410d6c6c1b836f8c03d7a9d67efc9b3
pedro <pedromunhozrosin@gmail.com>
2025-08-18 16:47:37 -0300

    adicionando pequeno codigo de teste

--- sceneDetection.py
+++ sceneDetection.py
@@ -5,7 +5,18 @@ import os
 
 #Cria uma pasta para colocar os arquivos de print da tela:
 #OBS: Atualmente servirá apenas para colocar imagens para teste ainda nao vai fazer a captura.
+'''
 dir = "Gameplay"
 os.makedirs(dir, exist_ok=True)
+'''
 
-#Deteccao de objetos na cena:
+from ultralytics import YOLO
+
+#carrega o modelo pre treinado do yolo
+model = YOLO('yolov8x.pt')  # versão pequena, rápida
+
+#faz a detecao a partir de uma imagem (por enquanto hehe)
+results = model('bf.png')
+
+#mostra a primeira resolucao
+results[0].show()  
